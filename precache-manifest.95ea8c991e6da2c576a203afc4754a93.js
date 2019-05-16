cafe250f0eb20f150523d75e1c2199d3e188ad24
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-05-17 02:00:27 +0300

    Updates

---
+++ precache-manifest.95ea8c991e6da2c576a203afc4754a93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2588a43d51eef320fa0ae7514f82b08d",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "e3f65d0913753a18d3f0",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e3f65d0913753a18d3f0",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.cc78e22b.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
